3ac623a16d3e5e469b6f34ca0528c623c8111cd8
Deb Kalyan Mohanty <debkalyanmohanty.id@gmail.com>
2022-10-31 23:37:37 +0530

    Solution Added

---
+++ Day-1 (31st Oct)/solution/DebKMohanty.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class DebKMohanty {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i =1 ;i<=n;i++){
+            double grade = sc.nextDouble();
+            double nm5 = grade%5 ==0 ? Math.floor(grade/5)*5 : (Math.floor(grade/5)*5) +5;
+            System.out.println(nm5);
+            if(grade >= 38 &&nm5-grade < 3.0  ){
+                grade = nm5;
+            }
+            System.out.println(grade);
+
+        }
+    }
+}
