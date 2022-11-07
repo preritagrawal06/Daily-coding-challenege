18dd9d33927841e80ea5d8428c264ede2a83111d
Deb Kalyan Mohanty <debkalyanmohanty.id@gmail.com>
2022-11-07 22:02:08 +0530

    Solution Added

---
+++ Day-4 (3rd Nov)/Solution/DebKMohanty.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DebKMohanty {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            count ^= sc.nextInt();
+        }
+        System.out.println(count);
+    }
+}
