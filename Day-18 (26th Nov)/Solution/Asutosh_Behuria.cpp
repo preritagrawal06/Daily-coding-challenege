47ab97ccb38946814edb05b98674391cc5839db2
asutosh-147 <98374996+asutosh-147@users.noreply.github.com>
2022-11-26 23:51:08 +0530

    day-18

--- Day-18 (26th Nov)/Solution/Asutosh_Behuria.cpp
+++ Day-18 (26th Nov)/Solution/Asutosh_Behuria.cpp
@@ -7,6 +7,7 @@ public:
                 if(nums[i]+nums[j]==target){
                     v.push_back(i);
                     v.push_back(j);
+                    break;
                 }
             }
         }
