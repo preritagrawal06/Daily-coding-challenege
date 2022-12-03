1ca807695f6bbda0776ed63d924ea9eaa82d88da
preritjn <preritbgr3010@gmail.com>
2022-12-03 15:33:47 +0530

    Sloution for Day 25

---
+++ Day-25 (3rd Dec)/Solutions/PreritKumarJain.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int lo=0,hi=nums.size()-1,mid,i;
+        while(lo<=hi) {
+            mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                hi=mid-1;
+            else
+                lo = mid+1;
+        }
+        return lo;
+    }
+};
