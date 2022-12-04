623334c235f85e62a54bf302137d25326c12f46c
Deb Kalyan Mohanty <debkalyanmohanty.id@gmail.com>
2022-12-04 21:47:47 +0530

    Day 20 Done

---
+++ November(Day 2- Day 22)/Day-20 (28th Nov)/Solutions/DebKMohanty.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int c =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[c++] = nums[i];
+            }
+        }
+        return c;
+    }
+}
